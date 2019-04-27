3456fd0020b025b5223d280654ea0e9a7b56594d
rick <chinmoy1113@gmail.com>
2019-04-27 15:43:55 +0530

    Initial Commit

---
+++ src/main/java/com/spring/hibernate/service/ManageEmployeeImpl.java
@@ -0,0 +1,49 @@
+package com.spring.hibernate.service;
+
+
+import com.spring.hibernate.model.Employee;
+import com.spring.hibernate.repository.EmployeeDAO;
+import java.util.List;
+
+public class ManageEmployeeImpl {
+    private static EmployeeDAO dao = null;
+
+    public static void main(String[] args) {
+        ManageEmployeeImpl me = new ManageEmployeeImpl();
+        ManageEmployeeImpl.dao = new EmployeeDAO();
+
+        Employee e1 = new Employee();
+        e1.setFirstName("Sara");
+        e1.setLastName("Ali");
+        e1.setSalary(100000);
+
+        Employee e2 = new Employee();
+        e2.setFirstName("Lisa");
+        e2.setLastName("Ann");
+        e2.setSalary(500000);
+
+        dao.save(e1);
+        dao.save(e2);
+
+        me.listAll("Initial");
+
+        e1.setLastName("Khan");
+        dao.update(e1);
+
+        me.listAll("Updated");
+
+        dao.delete(e1);
+
+        me.listAll("Deleted");
+
+    }
+
+    private void listAll(String message){
+        List<Employee> l2 = dao.getAll();
+        System.out.println("---------------"+message+" Listing----------------");
+        for(Employee e : l2){
+            System.out.println(e.getFirstName()+"\t"+e.getLastName()+"\t"+e.getSalary());
+        }
+    }
+
+}
