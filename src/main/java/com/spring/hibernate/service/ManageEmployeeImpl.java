e805e837aa3e6d02e7c32d671f58b9283f7dcb57
chinmoyd1 <chinmoy1113@gmail.com>
2019-05-03 20:16:24 +0530

    StandAlone Only

--- src/main/java/com/spring/hibernate/service/ManageEmployeeImpl.java
+++ src/main/java/com/spring/hibernate/service/ManageEmployeeImpl.java
@@ -4,10 +4,8 @@ package com.spring.hibernate.service;
 import com.spring.hibernate.config.SpringConfig;
 import com.spring.hibernate.model.Employee;
 import com.spring.hibernate.repository.EmployeeDAO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
